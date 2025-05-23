7303ba6f90a3aa9f996a36952a8b2588d4a725d4
Bradley Burgess <bradleyburgess@gmail.com>
2025-05-23 09:19:49 +0200

    Add Bookmark model; update User model

---
+++ src/LinkLeaf.Api/Models/Bookmark.cs
@@ -0,0 +1,11 @@
+namespace LinkLeaf.Api.Models;
+
+public class Bookmark
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public Guid UserId { get; set; }
+    public User User { get; set; } = default!;
+}
